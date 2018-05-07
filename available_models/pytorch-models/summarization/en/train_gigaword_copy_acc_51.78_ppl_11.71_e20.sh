5da699928c42959af109260e6e6e27c55b47e2a0
magdabslsk <magdalena.biesialska@upc.edu>
2018-05-07 06:25:22 +0200

    Add summarization Giga model

---
+++ available_models/pytorch-models/summarization/en/train_gigaword_copy_acc_51.78_ppl_11.71_e20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -p veu # Partition to submit to
+#SBATCH --mem=30G # Memory  
+#SBATCH --gres=gpu:4
+#SBATCH --ignore-pbs                                                            
+#SBATCH --output=train_gigaword_copy_acc_51.78_ppl_11.71_e20.log
+
+stdbuf -i0 -o0 -e0  python ../../../../translate.py -model gigaword_copy_acc_51.78_ppl_11.71_e20.pt -src ../data/Giga/input.txt -output pred_gigaword_copy_acc_51.78_ppl_11.71_e20.txt -replace_unk -verbose
